890feb5cb9a721e22f9f49b2c5532f72ea7d3f8f
MrEvplus <Mrevplus@gmail.com>
2025-07-29 23:16:57 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from mappa_leghe_supabase import run_mappa_leghe_supabase
 
 def get_league_mapping():
     try:
-supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
+    supabase = create_client(SUPABASE_URL, SUPABASE_KEY)
         data = supabase.table("league_mapping").select("*").execute().data
         return {r["code"]: r["league_name"] for r in data}
     except:
